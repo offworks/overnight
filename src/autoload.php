4a8c7c53dfb8a1ba612d864c9354c8796fc58a25
Ahmad Rahimie <newrehmi@gmail.com>
2016-06-12 03:35:39 +0800

    autoloader

---
+++ src/autoload.php
@@ -0,0 +1,15 @@
+<?php
+// autoload current folder with Overnight base namespace
+spl_autoload_register(function($class)
+{
+	$class = substr($class, strlen('Overnight\\'));
+
+	$class = str_replace('\\', '/', $class);
+
+	$path = __DIR__.'/'.$class.'.php';
+
+	if(file_exists($path))
+		require_once $path;
+});
+
+
